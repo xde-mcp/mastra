3eca6d95eb2a2b9eb6ca22eb45ace3c29f988cc1
dayo <ifedayoadeniyi@gmail.com>
2025-03-12 05:54:45 +0100

    Add data flow documentation (#2904)
    https://linear.app/kepler-crm/issue/MASTRA-2378/document-new-type-magics

--- docs/src/pages/docs/workflows/_meta.js
+++ docs/src/pages/docs/workflows/_meta.js
@@ -2,6 +2,7 @@ const meta = {
   "00-overview": "Overview",
   steps: "Steps",
   "control-flow": "Control Flow",
+  "data-flow": "Data Flow",
   variables: "Variables",
   "suspend-and-resume": "Suspend & Resume",
 };
