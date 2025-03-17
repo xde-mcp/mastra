eb55f7c1502dc8198c70553cae7c2aa0c8734d8e
dayo <ifedayoadeniyi@gmail.com>
2025-03-17 14:16:06 +0100

    Merge data flow content into control flow section (#2991)
    https://linear.app/kepler-crm/issue/MASTRA-2461/revise-workflows-documentation-taxonomy-for-clarity

--- docs/src/pages/docs/workflows/_meta.js
+++ docs/src/pages/docs/workflows/_meta.js
@@ -2,7 +2,6 @@ const meta = {
   "00-overview": "Overview",
   steps: "Steps",
   "control-flow": "Control Flow",
-  "data-flow": "Data Flow",
   variables: "Variables",
   "suspend-and-resume": "Suspend & Resume",
   "dynamic-workflows": "Dynamic Workflows",
