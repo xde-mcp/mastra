699c7d40ab295e4d0c35193eed8c78010f0dbfc1
Marvin Frachet <marvin.frachet@protonmail.com>
2025-03-18 18:06:45 +0100

    fix(docs): remove warning at build time about svg props formats (#3028)
    When building the docs project locally, you might see issues related to
    malformed svg props like `stroke-width` or `clip-rule`.
    
    This PR is just about renaming them to `strokeWidth` and `clipRule` that
    is the expected format for React.

--- packages/deployer/src/server/welcome.ts
+++ packages/deployer/src/server/welcome.ts
@@ -94,7 +94,7 @@ export const html = `
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
         >
           <path d="M5 12h14M12 5l7 7-7 7" />
         </svg>
