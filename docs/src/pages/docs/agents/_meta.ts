18654f61a2dff6ba9dece809f5dd0883dd99d1e7
Tyler Barnes <tylerdbarnes@gmail.com>
2025-03-19 15:43:06 -0700

    docs: initial MCP guide (#3045)
    This is a starting point - I'm going to expand on this in a follow up PR

--- docs/src/pages/docs/agents/_meta.ts
+++ docs/src/pages/docs/agents/_meta.ts
@@ -1,8 +1,9 @@
 const meta = {
   "00-overview": "Overview",
-  "01-agent-memory": "Memory",
-  "02-adding-tools": "Tools",
-  "03-adding-voice": "Voice",
+  "01-agent-memory": "Agent Memory",
+  "02-adding-tools": "Adding Tools",
+  "02a-mcp-guide": "Using MCP",
+  "03-adding-voice": "Adding Voice",
 };
 
 export default meta;
