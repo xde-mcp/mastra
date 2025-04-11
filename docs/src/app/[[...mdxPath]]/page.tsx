0f8a7fe655c19cc2cc74bfcdeb1cee46eed93246
Kenny <adelekekehinde06@gmail.com>
2025-04-11 11:20:05 +0100

    feat: add og image for docs (#3580)
    ## Description
    
    Fix broken og images after Nextra 4 migration
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [x] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [x] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have generated a changeset for this PR

--- docs/src/app/[[...mdxPath]]/page.tsx
+++ docs/src/app/[[...mdxPath]]/page.tsx
@@ -4,10 +4,40 @@ import { useMDXComponents as getMDXComponents } from "@/mdx-components";
 
 export const generateStaticParams = generateStaticParamsFor("mdxPath");
 
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
+
 export async function generateMetadata(props: any) {
   const params = await props.params;
   const { metadata } = await importPage(params.mdxPath);
-  return metadata;
+
+  const url = `${baseUrl}/${params.mdxPath.join("/")}`;
+  const title = metadata.title;
+  const description = metadata.description;
+
+  const image = metadata.image
+    ? metadata.image
+    : `${baseUrl}/api/og/docs?title=${title}&description=${description}`;
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      url,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+    },
+  };
 }
 
 const Wrapper = getMDXComponents().wrapper;
