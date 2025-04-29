3fd968cbb50bc4bc67549ed999b9f3be05e6df79
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-04-29 13:28:47 -0700

    Update agent ref doc (#3972)
    ## Description
    
    <!-- Provide a brief description of the changes in this PR -->
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [ ] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have generated a changeset for this PR

--- docs/src/content/en/reference/agents/_meta.ts
+++ docs/src/content/en/reference/agents/_meta.ts
@@ -1,8 +1,14 @@
 const meta = {
+  agent: "Agent",
   getAgent: "getAgent()",
   createTool: "createTool()",
   generate: "generate()",
   stream: "stream()",
+  getVoice: "getVoice()",
+  getInstructions: "getInstructions()",
+  getTools: "getTools()",
+  getModel: "getModel()",
+  getMemory: "getMemory()",
 };
 
 export default meta;
