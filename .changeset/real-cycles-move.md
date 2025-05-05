4214ae56a87266f9ab6bedd100b392c4537be544
Kenny <adelekekehinde06@gmail.com>
2025-05-05 18:01:50 +0100

    fix(mcp-docs-server): check if the latest docs server is broken (#4077)
    ## Description
    
    The `mcp-docs-server` was broken because `dirname` resolved to
    `undefined`. This fixes it.
    ![CleanShot 2025-05-05 at 15 18
    49@2x](https://github.com/user-attachments/assets/650de03d-91ac-4c2a-a39f-ce16cc330307)
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [x] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [x] I have generated a changeset for this PR
    
    ---------
    
    Co-authored-by: Abhi Aiyer <abhiaiyer91@gmail.com>
    Co-authored-by: Tyler Barnes <tylerdbarnes@gmail.com>

---
+++ .changeset/real-cycles-move.md
@@ -0,0 +1,5 @@
+---
+'@mastra/mcp-docs-server': patch
+---
+
+Fixed an issue where we were accessing `import.meta.dirname` which is only supported in newer node versions
--- packages/mcp-docs-server/src/utils.ts
+++ packages/mcp-docs-server/src/utils.ts
@@ -1,10 +1,13 @@
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export function fromRepoRoot(relative: string) {
-  return path.resolve(import.meta.dirname, `../../../`, relative);
+  return path.resolve(__dirname, `../../../`, relative);
 }
 export function fromPackageRoot(relative: string) {
-  return path.resolve(import.meta.dirname, `../`, relative);
+  return path.resolve(__dirname, `../`, relative);
 }
 
 // can't use console.log() because it writes to stdout which will interfere with the MCP Stdio protocol
