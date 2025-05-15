9273426b911cf16b1b76334f676fc7d557d1712c
Manthan Gupta <42516515+manthanguptaa@users.noreply.github.com>
2025-05-15 19:34:06 +0530

    Fix: broken examples of vnext workflows (#4234)
    ## Description
    
    - This PR fixes the issues in the vnext workflows examples 
    - Adds a new example for .foreach() helper function
    - Adds comments for better readability
    - Adds a setup step for each example
    - Removes broken GitHub links
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [x] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [ ] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have generated a changeset for this PR

--- docs/src/content/en/examples/workflows_vNext/_meta.ts
+++ docs/src/content/en/examples/workflows_vNext/_meta.ts
@@ -2,9 +2,10 @@ const meta = {
   "conditional-branching": "Conditional Branching",
   "parallel-steps": "Parallel Steps",
   "calling-agent": "Calling an Agent",
-  "agent-and-tool-interop": "Using a Tool/Agent as a Step",
+  "agent-and-tool-interop": "Tool & Agent as a Step",
   "human-in-the-loop": "Human in the Loop",
-  "control-flow": "Control flow",
+  "control-flow": "Control Flow",
+  "array-as-input": "Array as Input",
   // "input-data-mapping": "Input Data Mapping",
 };
 
