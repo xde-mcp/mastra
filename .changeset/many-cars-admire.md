992fe1759986637dfc768e79d4c68b54bc5d984b
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2025-05-27 11:20:07 -0700

    chore: remove mastra dep check from mastra lint command (#4488)

---
+++ .changeset/many-cars-admire.md
@@ -0,0 +1,5 @@
+---
+'mastra': patch
+---
+
+chore: remove check for alpha and non-alpha mastra packages from mastra lint
--- packages/cli/src/commands/lint/rules/index.ts
+++ packages/cli/src/commands/lint/rules/index.ts
@@ -1,7 +1,6 @@
 import { mastraCoreRule } from './mastraCoreRule.js';
-import { mastraDepsCompatibilityRule } from './mastraDepsCompatibilityRule.js';
 import { nextConfigRule } from './nextConfigRule.js';
 import { tsConfigRule } from './tsConfigRule.js';
 import type { LintRule } from './types.js';
 
-export const rules: LintRule[] = [nextConfigRule, tsConfigRule, mastraCoreRule, mastraDepsCompatibilityRule];
+export const rules: LintRule[] = [nextConfigRule, tsConfigRule, mastraCoreRule];
