86edfaeaff249a9e7b7be867a87be80454440c8d
Kehinde <adelekekehinde06@gmail.com>
2025-05-30 13:31:29 +0100

    fix: 404 link and bg of 404 in light mode (#4542)

--- docs/src/app/[locale]/not-found.tsx
+++ docs/src/app/[locale]/not-found.tsx
@@ -1,31 +1,31 @@
 "use client";
-import Link from "next/link";
-import "../globals.css";
-import { useParams, usePathname } from "next/navigation";
-import Image from "next/image";
-import Image404 from "../../../public/404-image.png";
 import { T, Var } from "gt-next";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image404 from "../../../public/404-image.png";
+import "../globals.css";
 
 export default function NotFound() {
   const pathname = usePathname();
-  const params = useParams();
-  const path = params?.mdxPath?.[0] || pathname.split("/")[1];
+  const path = pathname.split("/")[2];
+  console.log({ path });
 
   return (
-    <div className="bg-[var(--primary-bg)] relative min-h-screen w-full grid place-items-center text-white">
+    <div className="bg-primary relative min-h-screen w-full grid place-items-center text-white">
       <div className="text-center z-20 -mt-60">
         <h2 className="font-serif text-8xl text-[hsl(var(--tag-green))] font-medium">
           404
         </h2>
         <T id="_locale_.notFound.title">
-          <p className="font-semibold mt-5 text-[var(--color-el-4)]">
+          <p className="font-semibold mt-5 text-[var(--light-color-text-4)] dark:text-[var(--color-el-4)]">
             Sorry, we couldn&apos;t find that page
           </p>
         </T>
         <T id="_locale_.notFound.link">
           <Link
-            href="/docs"
-            className="font-semibold text-[var(--color-el-4)] mt-2"
+            href={`/${path}`}
+            className="font-semibold text-[var(--light-color-text-4)] dark:text-[var(--color-el-4)] mt-2"
           >
             Return to{" "}
             <span className="capitalize underline">
