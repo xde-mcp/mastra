4028db92b299ba17cae2005edf24698cea8ba947
Ward Peeters <ward@coding-tech.com>
2025-06-06 17:27:22 +0200

    fix test (#4779)

--- e2e-tests/pkg-outputs/bundle.test.ts
+++ e2e-tests/pkg-outputs/bundle.test.ts
@@ -86,10 +86,10 @@ describe.for(allPackages.map(pkg => [relative(join(__dirname.replaceAll('\\', '/
     });
 
     it.skipIf(
-      !pkgJson.name.startsWith('@mastra/') &&
-        pkgJson.name !== 'mastra' &&
-        pkgJson.name !== 'create-mastra' &&
-        pkgJson.name !== '@mastra/client-js',
+      pkgJson.name === 'mastra' ||
+        pkgJson.name === 'create-mastra' ||
+        pkgJson.name === '@mastra/client-js' ||
+        !pkgJson.name.startsWith('@mastra/'),
     )('should have @mastra/core as a peer dependency if used', async () => {
       const hasMastraCoreAsDependency = pkgJson?.dependencies?.['@mastra/core'];
       expect(hasMastraCoreAsDependency).toBe(undefined);
