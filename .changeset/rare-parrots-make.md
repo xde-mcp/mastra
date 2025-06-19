21ffb9774d1ac1b04663d95a1298a2693d1cb622
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2025-06-19 10:38:38 +0100

    make dynamic form handle schema better (#5230)
    ## Description
    
    <!-- Provide a brief description of the changes in this PR -->
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works

---
+++ .changeset/rare-parrots-make.md
@@ -0,0 +1,7 @@
+---
+'@mastra/playground-ui': patch
+'mastra': patch
+'create-mastra': patch
+---
+
+Make dynamic form handle schema better
--- packages/playground-ui/src/components/dynamic-form/index.tsx
+++ packages/playground-ui/src/components/dynamic-form/index.tsx
@@ -35,6 +35,7 @@ export function DynamicForm<T extends z.ZodSchema>({
   className,
   readOnly,
 }: DynamicFormProps<T>) {
+  const isNotZodObject = !(schema instanceof ZodObject);
   if (!schema) {
     console.error('no form schema found');
     return null;
@@ -44,20 +45,23 @@ export function DynamicForm<T extends z.ZodSchema>({
     if (isEmptyZodObject(schema)) {
       return z.object({});
     }
-    // using a non-printable character to avoid conflicts with the form data
-    return z.object({
-      '\u200B': schema,
-    });
+    if (isNotZodObject) {
+      // using a non-printable character to avoid conflicts with the form data
+      return z.object({
+        '\u200B': schema,
+      });
+    }
+    return schema;
   };
 
-  const schemaProvider = new CustomZodProvider(normalizedSchema(schema));
+  const schemaProvider = new CustomZodProvider(normalizedSchema(schema) as any);
 
   const formProps: ExtendableAutoFormProps<z.infer<T>> = {
     schema: schemaProvider,
     onSubmit: async values => {
-      await onSubmit?.(values?.['\u200B'] || {});
+      await onSubmit?.(isNotZodObject ? values['\u200B'] || {} : values);
     },
-    defaultValues: defaultValues ? { '\u200B': defaultValues } : undefined,
+    defaultValues: isNotZodObject ? (defaultValues ? { '\u200B': defaultValues } : undefined) : defaultValues,
     formProps: {
       className: '',
     },
