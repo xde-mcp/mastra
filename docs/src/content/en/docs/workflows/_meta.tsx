d65a9e06e6b9bed66fd361c0a92616e7b6546f20
Tony Kovanen <rase-@users.noreply.github.com>
2025-06-20 23:23:16 +0300

    Add docs for sleep/sleepUntil/waitForEvent (#5287)

--- docs/src/content/en/docs/workflows/_meta.tsx
+++ docs/src/content/en/docs/workflows/_meta.tsx
@@ -3,6 +3,7 @@ const meta = {
   overview: <Tag text="new">Overview</Tag>,
   "control-flow": "Control Flow",
   "suspend-and-resume": "Suspend & Resume",
+  "pausing-execution": "Sleep & Events",
   "input-data-mapping": "Input Data Mapping",
   "using-with-agents-and-tools": "Agents and Tools",
   "inngest-workflow": <Tag text="experimental">Inngest Workflow</Tag>,
