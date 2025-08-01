94f4812712a4b1ee7f6038c50944d5e6918340a8
Roamin <97863888+roaminro@users.noreply.github.com>
2025-08-01 07:57:30 -0400

    fix(workflow):  lazy initialize Run's `AbortController` (#6470)

---
+++ .changeset/many-parts-design.md
@@ -0,0 +1,5 @@
+---
+'@mastra/core': patch
+---
+
+lazy initialize Run's `AbortController`
--- packages/core/src/workflows/workflow.ts
+++ packages/core/src/workflows/workflow.ts
@@ -1210,7 +1210,7 @@ export class Workflow<
     const nestedAbortCb = () => {
       abort();
     };
-    run.abortController?.signal.addEventListener('abort', nestedAbortCb);
+    run.abortController.signal.addEventListener('abort', nestedAbortCb);
     abortSignal.addEventListener('abort', async () => {
       run.abortController.signal.removeEventListener('abort', nestedAbortCb);
       await run.cancel();
@@ -1337,7 +1337,7 @@ export class Run<
   TInput extends z.ZodType<any> = z.ZodType<any>,
   TOutput extends z.ZodType<any> = z.ZodType<any>,
 > {
-  public abortController: AbortController;
+  #abortController?: AbortController;
   protected emitter: EventEmitter;
   /**
    * Unique identifier for this workflow
@@ -1406,7 +1406,14 @@ export class Run<
     this.emitter = new EventEmitter();
     this.retryConfig = params.retryConfig;
     this.cleanup = params.cleanup;
-    this.abortController = new AbortController();
+  }
+
+  public get abortController(): AbortController {
+    if (!this.#abortController) {
+      this.#abortController = new AbortController();
+    }
+
+    return this.#abortController;
   }
 
   /**
